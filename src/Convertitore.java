4391940d8aa061ba23016ecdefb3f800064c2ce0
Ivan Migliore <migliore.ivan.27@gmail.com>
2020-04-19 11:34:44 +0200

    qualcosa

--- src/Convertitore.java
+++ src/Convertitore.java
@@ -1,12 +1,27 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 public class Convertitore {
-
+    private int conversioni;
 
     private JButton converti;
     private JTextField textField1;
+    private JRadioButton esadecimaleRadioButton;
+    private JRadioButton decimaleRadioButton;
+    private JRadioButton binarioRadioButton;
+    private JRadioButton ottaleRadioButton;
+
+    public Convertitore() {
+
+        converti.addActionListener(ActionEvent -> {
+
+        })
+    }
 
     public static void main(String[] args){
+
+
+
 
 
     }
