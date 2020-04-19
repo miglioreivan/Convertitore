4ae93ebba67f9a6f9bbfd14108493ea5ac631482
Ivan Migliore <migliore.ivan.27@gmail.com>
2020-04-19 18:36:45 +0200

    ae

--- src/Convertitore.java
+++ src/Convertitore.java
@@ -4,20 +4,26 @@ public class Convertitore {
     private int conversioni;
 
     private JButton converti;
-    private JTextField decimale;
+    private JTextField textField1;
     private JPanel conv;
     private JComboBox comboBox1;
     private JComboBox comboBox2;
+    private JTextField textField2;
 
     public Convertitore() {
         converti.addActionListener(a -> {
 
         });
-        comboBox1.addItem("EZ");
-        comboBox1.addItem("DIOPORCO");
-
-
-
+        comboBox1.addItem("");
+        comboBox1.addItem("HEX");
+        comboBox1.addItem("DEC");
+        comboBox1.addItem("OTT");
+        comboBox1.addItem("BIN");
+        comboBox2.addItem("");
+        comboBox2.addItem("HEX");
+        comboBox2.addItem("DEC");
+        comboBox2.addItem("OTT");
+        comboBox2.addItem("BIN");
 
     }
 
